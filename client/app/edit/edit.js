2b05a96e8804e905510e516bb7272fc8da83f88b
David Kae <davidskae@yahoo.com>
2014-12-04 12:34:48 -0800

    basic text box added

--- client/app/edit/edit.js
+++ client/app/edit/edit.js
@@ -1,5 +1,10 @@
 angular.module('waffle.edit', [])
 
 .controller('EditController', function ($scope, Edit) {
-	
+  $scope.submit = function() {
+  	Edit.addPost()
+  	.then (function() {
+  	  //can't test till ng-controller set up in main html?
+  	});
+  }
 })
