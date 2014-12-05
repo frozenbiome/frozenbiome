e5d70010401955f96ab459320d64ed79ae0f7f22
Alex Hawkins <alexhawkins.me@gmail.com>
2014-12-04 18:56:35 -0800

    Add dashboard template

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -2,7 +2,7 @@ angular.module('waffle.dashboard', [])
 
 
 .controller('DashboardController', function ($scope, Dashboard, $timeout) {
-  $scope.posts = ['hi'];
+  $scope.posts = [{url: 'www.google.com', title: 'Waffle House Printing Blog', content: 'asdjf ajsdljfasdlkfh ahsfjasdfjklasjdflaj asdlfjaksdfjasdflj laksdjflajdsf;l ja;sdkfjasdffjksadjfk lasdjfklasdjf ljsdf'}];
   $scope.getAllPosts = function () {
     Dashboard.getAllPosts()
       .then(function (data) {
