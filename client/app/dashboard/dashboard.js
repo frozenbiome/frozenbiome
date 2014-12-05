1779bd81b68bddab4d5c7dbdff921ebc79efd256
Alex Hawkins <alexhawkins.me@gmail.com>
2014-12-04 17:00:27 -0800

    Add dashboard controller

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -1,5 +1,17 @@
 angular.module('waffle.dashboard', [])
 
-.controller('DashboardController', function ($scope, Dashboard) {
 
-});
+.controller('DashboardController', function ($scope, Dashboard, $timeout) {
+  $scope.posts = [];
+  $scope.getAllPosts = function () {
+    Dashboard.getAllPosts()
+      .then(function (data) {
+         data.forEach(function(post) {
+          $scope.posts.push(post);
+        });
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+})
