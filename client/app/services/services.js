6c94e009616baaaef478b8d7a6e6dab255c6f472
Evan Spiler <monkeysrawesome@gmail.com>
2014-12-05 08:55:55 -0800

    add auth factory

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -16,17 +16,47 @@ angular.module('waffle.services', [])
 
 .factory('Dashboard', function ($http, $location, $window) {
   var getAllPosts = function () {
-  	return $http({
+    return $http({
       method: 'GET',
       //TODO: Dynamically update username, and display at top of dashboard page
       url: '/users?q=BILLY',
     })
     .then(function (res) {
-    	return res.data;
+      return res.data;
     });
   }
 
   return {
-  	getAllPosts: getAllPosts,
+    getAllPosts: getAllPosts,
+  };
+})
+
+.factory('Auth', function ($http, $location, $window) {
+  var login = function (username, password) {
+    return $http({
+      method: 'POST',
+      url: '/login',
+      data: {username: username, password: password}
+    })
+    //TODO display error responses to client
+    .then(function(res) {
+      
+    }) 
+  }
+
+  var signup = function () {
+    return $http({
+      method: 'POST',
+      url: '/signup',
+      data: {username: username, password: password}
+    })
+    //TODO display error responses to client 
+    .then(function(res) {
+    })
+  }
+
+  return {
+  	login: login,
+    signup: signup
   };
 })
