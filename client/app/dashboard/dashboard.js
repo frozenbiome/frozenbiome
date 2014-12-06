6fca4ca2928704202584b67a3be2eef176c860d2
Grant Wu <grantwu2@gmail.com>
2014-12-05 16:25:26 -0800

    Pre-pull commit

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -11,7 +11,6 @@ angular.module('waffle.dashboard', [])
       Dashboard.getAllPosts($rootScope.user)
       .then(function (data) {
         data.forEach(function(post) {
-          //TODO: PUSH IN DESC CREATED ORDER
           console.log(post)
           if ($scope.post_ids.indexOf(post._id) == -1) {
             $scope.posts.push(post);
