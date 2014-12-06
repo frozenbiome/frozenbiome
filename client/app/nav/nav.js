d83eadc08daa8be7dc64831d8ea77779f94e70eb
Evan Spiler <monkeysrawesome@gmail.com>
2014-12-06 15:34:21 -0800

    rootScope variables update on logout

--- client/app/nav/nav.js
+++ client/app/nav/nav.js
@@ -1,9 +1,12 @@
 angular.module('waffle.nav', [])
 
-.controller('NavController', function ($scope, $location, Auth) {
+.controller('NavController', function ($scope, $location, $rootScope, Auth) {
   $scope.logout = function() {
   	console.log("nav logging out")
   	Auth.logout();
+  	$rootScope.loggedIn = false;
+  	$rootScope.user = '';
+
   }
   // $scope.signedIn = Auth.signedIn;
   //$scope.post = {content: '', title: ''};
